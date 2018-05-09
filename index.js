1d39dd6ad148b12f9574de8ea2fb18dfad497c65
JaredRCooper <jaredrcoopet@gmail.com>
2018-05-08 22:36:11 -0400

    good place to commit, working on downloading/target images on i.reddit.

--- index.js
+++ index.js
@@ -3,41 +3,55 @@ const cheerio = require('cheerio');
 //const validator = require('validator-js');
 //built in node module that allows you to save and write files
 const fs = require('fs');
+const path = require('path');
 //pussypassdenied, 4chan, greentext, dankmemes
 let stuff = [];
 
 grabImage = (url) => {
-	request(url, function(error, response, html) {
+	request(url, function (error, response, html) {
 		if (!error && response.status == 200) {
-		let $ = cheerio.load(html);
-	let imgContainer = $(this).prev();
-	console.log(imgContainer);
-	}})
+			let $ = cheerio.load(html);
+			//sets img equal to div parent of image then goes to div below
+			let img = $('div.media-preview-content').next();
+			console.log(img);
+		}
+	})
 }
+
+
+//this function takes in a url as a string, a file path as a string, and a callback function
+/*
+const downloadImage = (url, path, callback) => {
+	request({uri: url})
+	.pipe(fs.createWriteStream(path))
+	.on('close', function() {
+		callback();
+	})
+} */
 
 scrapeSub = () => {
 	request('https://www.reddit.com/r/4chan', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    $('span.domain').each(function(i, element){
-      // Select the previous element
-      var a = $(this).prev();
-	  var title = a.text();
-	  var url = a.attr('href');
-	  let pattern = new RegExp(/\/r\//)
-	  if (pattern.test(url)) url = 'https://reddit.com'+url;
-	  stuff.push(/*title + ' ' +*/ url);
-	 // grabImage(url);
-	  // validator.equals(url, 'imgur');
-
+		if (!error && response.statusCode == 200) {
+			let $ = cheerio.load(html);
+			$('span.domain').each(function (i, element) {
+				// Select the previous element
+				var a = $(this).prev();
+				var title = a.text();
+				var url = a.attr('href');
+				let pattern = new RegExp(/\/r\//)
+				if (pattern.test(url)) url = 'https://reddit.com' + url;
+				stuff.push( /*title + ' ' +*/ url);
+			})
+			//map over the array of links and pass each into a link variable
+			stuff.map(link => {
+				if (link.includes('reddit.com')) {
+				}
+				grabImage(link);
+			})
+			console.log(stuff);
+		}
 	})
-	stuff.map(thing => {
-		if (thing.includes('reddit.com')) {
-
-		} grabImage(thing);
-	})
-	console.log(stuff);
-}})};
+};
 
 
 scrapeSub()
@@ -52,4 +66,4 @@ scrapeSub()
 
 //post to facebook every 15 minutes then delete picture
 
-//post the id of the 
+//post the id of the
